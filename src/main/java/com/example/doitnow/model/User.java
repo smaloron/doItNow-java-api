ad03743ed5a55080213151ed77dbb0e636068257
seb <contact@smaloron.eu>
2026-03-25 13:47:46 +0100

    update user related features

--- src/main/java/com/example/doitnow/model/User.java
+++ src/main/java/com/example/doitnow/model/User.java
@@ -4,10 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
@@ -18,9 +22,15 @@ import java.util.List;
 @Document(collection = "users")
 public class User implements UserDetails {
 
+    @Id
     private String id;
+
+    @Indexed(unique = true)
     private String email;
     private String password;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
--- src/main/java/com/example/doitnow/repository/UserRepository.java
+++ src/main/java/com/example/doitnow/repository/UserRepository.java
@@ -14,4 +14,6 @@ public interface UserRepository extends MongoRepository<User, String> {
 
     // Vérifie si un utilisateur avec cet email existe déjà
     boolean existsByEmail(String email);
+
+
 }
